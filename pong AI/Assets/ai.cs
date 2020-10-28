1bdb7234b508dc19573f8b1ef0d2ec09b0285619
Julian <Julian.frost101@gmail.com>
2020-10-28 14:17:36 -0400

    pong ai ball bugged in -y direction will fix later

---
+++ pong AI/Assets/ai.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ai : MonoBehaviour
+{
+    public Transform ball;
+
+    [Range(0, 1)]
+    public float skill;
+
+    void Update()
+    {
+
+        Vector3 pos = transform.position;
+        pos.y = Mathf.Lerp(transform.position.y, ball.position.y, skill);  //Linearly interpolates 
+        transform.position = pos;
+    }
+
+}
+
+//https://www.youtube.com/watch?v=jKbZM3KLqVI
---
+++ pong AI/Assets/ball.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public float speed = .1f;
+    Vector3 velocity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        float derpx = Random.Range(0, 2) * 2f - 1f;
+        float derpy = Random.Range(0, 2) * 2f - 1f * Random.Range(0f,0f); //pick btw 0 and 1 if zero assign sign to it
+
+        //GetComponent<Rigidbody>().velocity = new Vector3(derpx * speed, derpy, 0f);
+        velocity = new Vector3(derpx, derpy, 0);
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        velocity = velocity.normalized * speed;
+        transform.position += velocity;
+        
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.transform.name)
+        {
+            case "top":
+            case "bottom":
+            velocity.x *= -1f;
+                return;
+        }
+
+
+    }
+
+
+
+
+
+
+}
+
+//https://www.youtube.com/watch?v=jKbZM3KLqVI
